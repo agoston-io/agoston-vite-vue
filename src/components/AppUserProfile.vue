119157f02f594da7a4e6f8564b7b81686bca077b
nico <nico@bastion.agoston.io>
2023-10-31 16:56:49 +0100

    progress

--- src/components/AppUserProfile.vue
+++ src/components/AppUserProfile.vue
@@ -1,45 +1,45 @@
 <template>
     <div class="container mt-5 py-5">
         <div class="row justify-content-center mt-5">
-            <div class="col-5">
+            <div class="col-8">
                 <h2 class="mt-5 mb-5">User profile</h2>
 
                 <div class="row">
-                    <div class="col-6 fw-bold">
+                    <div class="col-4 fw-bold">
                         User id
                     </div>
-                    <div class="col-6">
+                    <div class="col-8">
                         {{ $session.user_id }}
                     </div>
                 </div>
 
                 <div class="row">
-                    <div class="col-6 fw-bold">
+                    <div class="col-4 fw-bold">
                         Username
                     </div>
-                    <div class="col-6">
+                    <div class="col-8">
                         {{ $session.auth_subject }}
                     </div>
                 </div>
 
 
                 <div class="row">
-                    <div class="col-6 fw-bold">
+                    <div class="col-4 fw-bold">
                         Is authenticated
                     </div>
-                    <div class="col-6">
+                    <div class="col-8">
                         {{ $session.is_authenticated }}
                     </div>
                 </div>
 
 
                 <div class="row">
-                    <div class="col-6">
+                    <div class="col-4">
                         <span class="fw-bold">Session data</span>
-                        <span> (comming from the authentication provider)</span>
+                        <span> - coming from the authentication provider and usable in your frontend</span>
                     </div>
-                    <div class="col-6">
-                        {{ $session }}
+                    <div class="col-8">
+                        <code><pre id="json">{{ JSON.stringify($session, null, 4) }}</pre></code>
                     </div>
                 </div>
 
